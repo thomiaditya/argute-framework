f14f7077400c0d7b64caaa74951aca47f3d5271c
Thomi Aditya Alhakiim <thomiaditya@gmail.com>
2020-02-02 18:08:03 +0700

    Adding comments to Parameter Class

--- src/anamorph/Important/Container/Parameter.php
+++ src/anamorph/Important/Container/Parameter.php
@@ -9,18 +9,36 @@ class Parameter
     /**
      * Hold the container.
      *
-     * @var [type]
+     * @var Anamorph\Covenant\Container\Container
      */
     protected $container;
 
+    /**
+     * Contains the abstract that used.
+     *
+     * @var string
+     */
     protected $abstract;
 
+    /**
+     * Add the container and the abstract properties.
+     *
+     * @param Anamorph\Covenant\Container\Container $container
+     * @param string $abstract
+     */
     public function __construct(Container $container, $abstract)
     {
         $this->container = $container;
         $this->abstract = $abstract;
     }
 
+    /**
+     * Add params to define class.
+     *
+     * @param array $parameters
+     * 
+     * @return void
+     */
     public function withParams(array $parameters)
     {
         $this->container->addToParams($this->abstract, $parameters);
